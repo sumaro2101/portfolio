c6d52a8c90c5a06c2d0f38c8be7638d762b08920
alex <yukiu217@gmail.com>
2023-01-23 10:55:36 +0200

    added block 'about'

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,8 +3,9 @@ const hamburger = document.querySelector('.hamburger'),
       closeElem = document.querySelector('.menu__cancel');
 
 hamburger.addEventListener('click', () => {
-  menu.classList.add('active');
+    menu.classList.add('active');
 });
+
 closeElem.addEventListener('click', () => {
-  menu.classList.remove('active');
+    menu.classList.remove('active');
 });
