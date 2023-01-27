9d3e455b33967a42fbb0db1fd05745bbba1f919f
alex <yukiu217@gmail.com>
2023-01-27 12:23:11 +0200

    added megia and finish fix

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -16,3 +16,33 @@ const counters = document.querySelectorAll('.use__stat-procent'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+$(document).ready(function() {
+        $('#form').validate({
+            rules: {
+            name: {
+                required: true,
+                minlength: 2 
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            checkbox: {
+                required: true
+            }
+            },
+            messages: {
+            name: {
+                required: "Заполните эту строчку!",
+                minlength: "Введите миниум {0} символа!"
+            },
+            email: {
+                required : "Введите электронный адресс!",
+                email: "Введите электронный адресс имеющий символ '@'"
+            },
+            checkbox: {
+                required: "Отметьте галочкой!"
+            }
+        }
+    });  
+});
